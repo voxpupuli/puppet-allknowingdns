1f6e5be193c623afa5c79a3d05937b0357ad56ad
Sebastien Badia <seb@sebian.fr>
2015-02-27 16:58:09 +0100

    spec: prepare specs for rspec 3.x and puppet-rspec 2.x

--- spec/classes/allknowingdns_spec.rb
+++ spec/classes/allknowingdns_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'shared_examples'
 
 describe 'allknowingdns', :type => :class do
 
@@ -13,18 +14,18 @@ describe 'allknowingdns', :type => :class do
   describe 'input validation' do
     describe 'invalid upstream' do
       let(:params) { params_set.merge({ :upstream => '1.2.3.4.1' }) }
-      it { expect { subject }.to raise_error(Puppet::Error, /is not a valid IP address/)}
+      it_raises 'a Puppet::Error', /is not a valid IP address/
     end
 
     describe 'invalid address' do
       #https://github.com/puppetlabs/puppetlabs-stdlib/blob/master/lib/puppet/parser/functions/is_domain_name.rb
       let(:params) {{ :address => 'wrong%domain.net' }}
-      it { expect { subject }.to raise_error(Puppet::Error, /is not a valid domaine name/)}
+      it_raises 'a Puppet::Error', /is not a valid domaine name/
     end
   end
 
-  it { should contain_package('all-knowing-dns') }
-  it { should contain_service('all-knowing-dns').with(
+  it { is_expected.to contain_package('all-knowing-dns') }
+  it { is_expected.to contain_service('all-knowing-dns').with(
     :ensure     => 'running',
     :enable     => true,
     :hasstatus  => true,
@@ -32,7 +33,7 @@ describe 'allknowingdns', :type => :class do
     :require    => 'Package[all-knowing-dns]'
   )}
 
-  it { should contain_file('/etc/all-knowing-dns.conf').with(
+  it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with(
      :ensure   => 'file',
     :owner    => 'root',
     :group    => 'root',
@@ -44,21 +45,21 @@ describe 'allknowingdns', :type => :class do
   context 'With params' do
     let(:params) { params_set }
 
-    it { should contain_file('/etc/all-knowing-dns.conf').with_content(/listen #{params_set[:listen][0]}/)}
-    it { should contain_file('/etc/all-knowing-dns.conf').with_content(/listen #{params_set[:listen][1]}/)}
-    it { should contain_file('/etc/all-knowing-dns.conf').with_content(/network #{params_set[:network]}/)}
-    it { should contain_file('/etc/all-knowing-dns.conf').with_content(/resolves to ipv6-%DIGITS%.#{params_set[:address]}/)}
+    it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/listen #{params_set[:listen][0]}/)}
+    it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/listen #{params_set[:listen][1]}/)}
+    it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/network #{params_set[:network]}/)}
+    it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/resolves to ipv6-%DIGITS%.#{params_set[:address]}/)}
     context 'Without prefix address' do
       let(:params) { params_set.merge({ :address_prefix => 'UNSET' }) }
-      it { should contain_file('/etc/all-knowing-dns.conf').with_content(/resolves to %DIGITS%.#{params_set[:address]}/)}
+      it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/resolves to %DIGITS%.#{params_set[:address]}/)}
     end
     context 'With prefix address' do
       let(:params) { params_set.merge({ :address_prefix => 'adsl.ipv6.' }) }
-      it { should contain_file('/etc/all-knowing-dns.conf').with_content(/resolves to adsl.ipv6.%DIGITS%.#{params_set[:address]}/)}
+      it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/resolves to adsl.ipv6.%DIGITS%.#{params_set[:address]}/)}
     end
     context 'With upstream' do
       let(:params) { params_set.merge({ :upstream => '2001:4d88:100e:1::2' }) }
-      it { should contain_file('/etc/all-knowing-dns.conf').with_content(/with upstream #{params_set[:upstream]}/)}
+      it { is_expected.to contain_file('/etc/all-knowing-dns.conf').with_content(/with upstream #{params_set[:upstream]}/)}
     end
   end
 end
